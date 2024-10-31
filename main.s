399363533d7c671d803a40362ed0e718ba32fa28
NaX0joL <167953280+NaX0joL@users.noreply.github.com>
2024-10-31 17:53:03 +0900

    Update main.s
    added syscall interface.

--- main.s
+++ main.s
@@ -115,6 +115,41 @@ boot:
 
 	bra MAIN
 
+
+*************************************
+** SYSTEM CALL INTERFACE
+**###################################
+** input:
+**	d0: system call number
+**	d1~: system call arg
+** output:
+** 	d0: the call result
+*************************************
+SYSCALL:
+	cmpi.l	#SYSCALL_NUM_GETSTRING, %d0
+	beq	CALL_GETSTRING
+	cmpi.l	#SYSCALL_NUM_PUTSTRING, %d0
+	beq	CALL_PUTSTRING
+	cmpi.l	#SYSCALL_NUM_RESET_TIMER, %d0
+	beq	CALL_RESET_TIMER
+	cmpi.l	#SYSCALL_NUM_SET_TIMER, %d0
+	beq	CALL_SET_TIMER
+	rte
+	
+CALL_GETSTRING:
+	jsr	GETSTRING
+	rte
+CALL_PUTSTRING:
+	jsr	PUTSTRING
+	rte
+CALL_RESET_TIMER:
+	jsr	RESET_TIMER
+	rte
+CALL_SET_TIMER:
+	jsr	SET_TIMER
+	rte
+
+
 ****************************************************************
 ** Interrupt Controller
 ****************************************************************
