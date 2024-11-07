9bb2f0ded059cceeca4ba7f862ed67e15a0c46ee
m13e50 <emiben1420@gmail.com>
2024-11-07 16:23:13 +0900

    PUTSTRING updated.

--- PUTSTRING.s
+++ PUTSTRING.s
@@ -1,52 +1,47 @@
+	***************************************
+	**INPUT:
+	**%d1.l:ch=0|1
+	**%d2.l:p
+	**%d3.l: data size to be sent
+	**OUTPUT:	
+	**%d0= number of bytes of data written in queue(actually sent)
+	***************************************
 .section .text
 PUTSTRING:
-	/*INPUT:
-	%d1.l:ch=0|1
-	%d2.l:p
-	%d3.l: data size to be sent
-	OUTPUT:	
-	%d0= number of bytes of data written in queue(actually sent)
-	*/
 	cmpi.l	#0, %d1 /* if ch!=0 -> no exec*/
 	beq PUTSTRING_INIT 
 	rts
 	
 
 PUTSTRING_INIT:
-	movem.l %a1-%a6,-(%sp)
-	bne PUT_STOP
+	/* sz<- 0, i<-p*/
 	move.l #0, size_put/*size of data put to the Q*/
 	move.l size_put,%d0/*D0.l:= number of data sz actually sent*/
 	move.l %d2, ptr_put /*start address of data to be transmitted (in receiver Q)*/
 	cmp #0,%d3 /*number of data to be sent: receiverQ size?*/
 	beq PUT_STOP
-	movem.l (%sp)+, %a1-%a6
-	rts
 
 PUTSTRING_DO:
 	cmp size_put, %d3
 	beq PUT_UNMASK
-	jsr PUT_DATA
-PUT_UNMASK:
-	ori.w 0xe107, %SR
-	bra PUT_STOP
-PUT_STOP:
-	move.l size_put, %d0
-	movem.l  (%sp)+,%a1-%a7
-	rts
+
 PUT_DATA:
 	movem.l %d0-%d1/%a1,-(%sp)
 	move.l ptr_put,%a1/*d1:= pointer in receiver Q to get data*/
-	moveq.l #1, %d0/*choose trans. queue*/
-	move.b (%a1)+, %d1 /* moves content in addr. p to d1*/
+	moveq.l #1, %d0 /*choose trans. queue*/
+	move.b (%a1)+, %d1 /* moves content in addr. p to d1 AND i++*/
 	jsr INQ
+	cmp #0, %d0
 	lea.l %a1, ptr_put /*update the pointer*/
 	jsr UPDATE_SZ
-	cmp size_put,%d3
-	beq PUT_UNMASK
-	cmp #0, %d0
-	beq PUT_UNMASK
-	bra PUT_DATA
+	bra PUTSTRING_DO
+PUT_UNMASK:
+	ori.w 0x0007, USTCNT1
+	bra PUT_STOP
+PUT_STOP:
+	move.l size_put, %d0
+	rts
+
 
 UPDATE_SZ:
 	move.l %d2,-(%sp)
