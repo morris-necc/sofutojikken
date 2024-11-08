f364863d7a9b3aa0d80524a95bddbf332172ecc6
morris-necc <71365308+morris-necc@users.noreply.github.com>
2024-11-08 15:19:31 +0900

    Update GETSTRING.s

--- GETSTRING.s
+++ GETSTRING.s
@@ -1,36 +1,34 @@
 GETSTRING:
 	/* Input: ch -> d1, head address of destination p -> d2, no. of data to be read -> d3 */
 	/* Output: no. of data actually read out -> d0 */
-	movem.l	%d4-%d5/%a0, -(%sp)
+	movem.l	%d4/%a0, -(%sp)
 
-	cmp	#0, %d1
+	cmpi.l	#0, %d1
 	bne	GETSTRING_END	/* If ch =/= 0, end */
 
-	move.l	#0, %d4		/* d4 = sz */
-	move.l	%d2, %d5	/* d5 = i */
+	move.l	#0, %d4		/* d4 = sz (Used to count no. of data actually read out) */
+	move.l	%d2, %a0	/* a0 = i (NOT Index, but head address of destination) */
 	
 
 GETSTRING_LOOP:
-	cmp	%d4, %d3
+	cmp	%d4, %d3	/* is sz == size? */
 	beq	GETSTRING_END
 
 	move.l	#0, %d0		/* specify queue 0 */
-	jsr	OUTQ		/* Call OUTQ */
+	jsr	OUTQ		/* Call OUTQ, puts data in d1 */
 
-	cmp	#0, %d0			/* If failure */
+	cmpi.l	#0, %d0			/* If failure */
 	beq	GETSTRING_UPD_SZ	/* End GETSTRING */
 
-	move.l	%d5, %a0
-	move.l	%d1, (%a0)	/* Copy the data to address i */
+	move.b	%d1, (%a0)	/* Copy the data to address i */
 	
 	addq	#1, %d4		/* Increment sz and i */
-	addq	#1, %d5
+	addq	#1, %a0
 	jmp	GETSTRING_LOOP
 
 GETSTRING_UPD_SZ:	
 	move.l	%d4, %d0	/* %d0 <- sz */
 	
 GETSTRING_END:
-	movem.l	(%sp)+, %d4-%d5/%a0
+	movem.l	(%sp)+, %d4/%a0
 	rts
-	
