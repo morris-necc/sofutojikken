d52be250170e822e8efeded3ca4138f43fe7f9b5
morris-necc <71365308+morris-necc@users.noreply.github.com>
2024-11-12 17:53:00 +0900

    Update interrupt_interface.s

--- interrupt_interface.s
+++ interrupt_interface.s
@@ -1,16 +1,17 @@
 INTERFACE:
 	movem.l	%d0-%d3,-(%sp)
 	
-	/* Transmitter Interrupt */
-	move.l	UTX1,	%d0
-	btst.b	#15, %d0	/* Transmitter FIFO empty? 1 = empty, 0 = not empty*/
-	beq	CALL_INTERPUT	/* not equal to 1*/
-	
 	/* Receiver Interrupt */
 	move.w	URX1, %d3	/* Copy register URX1 to %d3.w*/
-	move.b	%d3, %d2	/* Copy lower 8 bits (data part) of %d3.w to %d2.b*/
-	btst.b	#13, %d3 	/* Receiver FIFO? 1 = not empty, 0 = empty, yes it's confusing*/ 
-	beq	CALL_INTERGET	/* Basically, this checks if it is a receiver interupt*/
+	btst.l	#13, %d3 	/* Receiver FIFO? 1 = not empty, 0 = empty, yes it's confusing*/ 
+	bne	CALL_INTERGET	/* Basically, this checks if it is a receiver interupt*/
+	
+	
+	/* Transmitter Interrupt */
+	move.w	UTX1, %d3
+	btst.l	#15, %d3	/* Transmitter FIFO empty? 1 = empty, 0 = not empty*/
+	bne	CALL_INTERPUT	/* not equal to 1*/
+	
 	
 INTERFACE_END:	
 	movem.l	(%sp)+, %d0-%d3
@@ -19,9 +20,10 @@ INTERFACE_END:
 CALL_INTERPUT:
 	move.l	#0, %d1
 	jsr	INTERPUT
-	jmp	INTERFACE_END
+	bra	INTERFACE_END
 
 CALL_INTERGET:
 	move.l	#0, %d1
+	move.b	%d3, %d2	/* Copy lower 8 bits (data part) of %d3.w to %d2.b*/
 	jsr	INTERGET
-	jmp	INTERFACE_END
+	bra	INTERFACE_END
