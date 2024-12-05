22debd93e5ac2031fa7f2ce6f75eb2242728dab4
limliangs <99681896+limliangs@users.noreply.github.com>
2024-12-05 16:08:27 +0900

    added semaphore asm

--- jikken2/theme2/mtk/mtk_as.s
+++ jikken2/theme2/mtk/mtk_as.s
@@ -1,13 +1,25 @@
 .include "equdefs.inc"
 .section .text
+	.global pv_handler
+	.global P
+	.global V
 
+	.extern p_body
+	.extern v_body
 	.extern curr_task
 	.extern addq
 	.extern sched
 	.extern ready /*points to the first task*/
 	.extern task_tab
 	.extern next_task
+ 
 
+********************
+** System call numbers 
+******************** 
+	.equ    SYSCALL_P, 0
+	.equ    SYSCALL_V, 1
+ 
 swtch:
 	move.w %SR , -(%sp)/*SR is piled up on the stack so that the process can be returned by the RTE.*/
 	
@@ -127,3 +139,51 @@ init_timer:	//clock interrupt routine: generates hardware interruption by the ti
 	trap	#0
 
 	movem.l (%sp)+,%d0-%d2
+
+********************************
+** Entrance of P system call
+** Input: D1 = semaphore ID
+********************************
+P:
+	movem.l	%d0-%d1, -(%sp)
+	move.l	SYSCALL_P, %d0
+	move.l	12(%sp), %d1
+	trap	#1
+	movem.l	(%sp)+, %d0-%d1
+	rts
+	
+********************************
+** Entrance of V system call
+** Input: D1 = semaphore ID
+********************************
+V:
+	movem.l	%d0-%d1, -(%sp)
+	move.l	SYSCALL_V, %d0
+	move.l	12(%sp), %d1
+	trap	#1
+	movem.l	(%sp)+, %d0-%d1
+	rts
+
+********************************
+** TRAP #1 interrupt provessing routine
+** D0 = P/V system call
+** D1 = Semaphore ID
+** According to D0, call p_body() or v_body()
+********************************
+pv_handler:
+	movem.l	%d1, -(%sp)	/* save argument on top of stack */
+	move.w	#0x2700, %SR
+	cmp	#0, %d0
+	beq	CALL_P_BODY
+	cmp	#1, %d0
+	beq 	CALL_V_BODY
+	bra	end_pv_handler
+CALL_P_BODY:
+	jsr	p_body
+	bra 	end_pv_handler
+CALL_V_BODY:	
+	jsr	v_body
+	bra	end_pv_handler
+end_pv_handler:
+	movem.l	(%sp)+, %d1
+	rte
