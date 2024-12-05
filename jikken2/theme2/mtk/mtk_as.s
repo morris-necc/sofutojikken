9f77372e68ca552c96d79b51d4d59a4baddf5be7
m13e50 <114471192+m13e50@users.noreply.github.com>
2024-12-05 14:55:17 +0900

    finish hard_clock

--- jikken2/theme2/mtk/mtk_as.s
+++ jikken2/theme2/mtk/mtk_as.s
@@ -4,6 +4,18 @@
 	.extern curr_task
 	.extern addq
 	.extern sched
+	.extern ready /*points to the first task*/
+
+	*****************************************
+	** subroutine first_task
+	** To start user task: stack used by kernel is switched to the stack pointed by "curr_task"
+	** activated once (with "begin_sch()")
+	** ends with RTE
+	** needs to be in supervisor mode
+	*****************************************
+first_stack:
+	
+	
 	******************************************
 	*** create the timer subroutines:
 	*** init_timer/Trap#0:OK/set_timer:OK/reset_timer:OK/hard_clock
@@ -14,18 +26,25 @@ hard_clock: // timer interrupt routine
 	/*to check if supervisor mode*/
 	move.l %sp, %a1
 	adda.l #12,%a1
-	move.l (%a1),%d1
+	move.l (%a1),%d1//get SR value to %d1
 	addi.l #2000,%d1
-	cmpi.l #2000,%d1
+	cmpi.l #2000,%d1 //check if supervisor mode
 	beq hard_clock_end
 	//add "curr_task" to the end of 'ready' using addq()
+	move.l curr_task,-(%sp)
+	move.l #ready, -(%sp)
+	jsr addq
+	addq.l #8, %sp
+
 	//start "sched": the ID of task to be executed next='next_task'
+	jsr sched
 	//start 'swtch'
-	//restoration of register
+	jsr swtch
+	movem.l (%sp)+, %d0-%d1/%a1 //restoration of register
 	rts
 
 hard_clock_end:
-	
+	movem.l (%sp)+, %d0-%d1/%a1
 	rts
 
 init_timer:	//clock interrupt routine: generates hardware interruption by the timer control routine(created in jikken1): interruption period: 1s)
