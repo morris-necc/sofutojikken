96b13252df0b51979e1770c6f61e12b38c6447aa
Emi21 <114471192+m13e50@users.noreply.github.com>
2024-12-05 16:49:57 +0900

    change test2

--- jikken2/theme2/mtk/test2.c
+++ jikken2/theme2/mtk/test2.c
@@ -2,6 +2,13 @@
 #include "mtk_c.h"
 #define MAX 1024
 
+void task1(){ 
+    // task1 definition 
+} 
+void task2(){ 
+    // task2 definition 
+}
+
 void main(){
   //hardware initialization
   init_kernel(); // maybe?
