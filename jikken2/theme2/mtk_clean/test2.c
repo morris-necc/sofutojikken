81178e4a1acfc5adf330d73eb94d0ae81dfef4c0
morris-necc <71365308+morris-necc@users.noreply.github.com>
2024-12-06 16:05:31 +0900

    Update test2.c

--- jikken2/theme2/mtk_clean/test2.c
+++ jikken2/theme2/mtk_clean/test2.c
@@ -2,20 +2,40 @@
 #include "mtk_c.h"
 #define MAX 1024
 
-void task1(){ 
+void task1(){
+  P(1);
+  for(int i = 1; i <= 20; ++i){
+    printf("\n1 %d", i);
+  }
+  V(1);
   while(1){
-    P(0);
-    //Something here?
-    V(0);}
+    printf("\ntask1 finished");
+  }
 }
 
-void task2(){ 
+void task2(){
+  P(1);
+  for(int i = 1; i <= 20; ++i){
+    printf("\n1 %d", i);
+  }
+  V(1);
   while(1){
-    P(0);
-    //Something here?
-    V(0);
-    }
+    printf("\ntask2 finished");
+  }
 }
+
+
+void task3(){
+  P(1);
+  for(int i = 1; i <= 20; ++i){
+    printf("\n1 %d", i);
+  }
+  V(1);
+  while(1){
+    printf("\ntask3 finished");
+  }
+}
+
 
 int main(){
   //hardware initialization
@@ -24,6 +44,7 @@ int main(){
   //system setting
   set_task(task1);
   set_task(task2);
+  set_task(task3);
   begin_sch(); //start multitasking
   return 0;
 }
