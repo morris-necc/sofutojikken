a94607504779c3eecceca2369d66ab5e4efddf2e
morris-necc <71365308+morris-necc@users.noreply.github.com>
2024-12-06 17:26:02 +0900

    Update mtk_asm.s

--- jikken2/theme2/mtk_clean/mtk_asm.s
+++ jikken2/theme2/mtk_clean/mtk_asm.s
@@ -28,14 +28,14 @@
 swtch:
 	move.w 	%SR , -(%sp)/*SR is piled up on the stack so that the process can be returned by the RTE.*/
 	
-	movem.l %d0-%d7/%a0-%a7,-(%sp)/*Saving register of task under execution*/
+	movem.l %d0-%d7/%a0-%a6,-(%sp)/*Saving register of task under execution*/
 	move.l 	%USP, %a1
 	move.l 	%a1, -(%sp)
 
 	move.l 	curr_task,%d0 	/*current task ID*/
 	lea.l 	task_tab, %a0 	/*save the pointer to the beginning of task_tab*/
 	mulu 	#20, %d0 	/* because every element takes 4*5 bytes*/
-	addq.l 	#4, %d0 		/*to access stack_ptr*/
+	add.l 	#4, %d0 	/*to access stack_ptr*/
 	adda.l 	%d0, %a0 	/*access the stack_ptr of curr_task in task_tab*/
 	move.l 	%sp, (%a0) 	/* record the SSP*/
 
@@ -54,7 +54,7 @@ swtch:
 	/*Read out register of next task*/
 	move.l 	(%sp)+, %a1
 	move.l 	%a1, %USP
-	movem.l (%sp)+, %d0-%d7/%a0-%a7
+	movem.l (%sp)+, %d0-%d7/%a0-%a6
 
 	rte
 
@@ -66,6 +66,8 @@ swtch:
 ** needs to be in supervisor mode
 *****************************************
 first_task:
+	move.w	#0x2700, %SR
+
 	/* calcuate TCB's head address */
 	/* find the address of TCB of 'curr_task' */
 	clr.l	%d1
@@ -76,15 +78,14 @@ first_task:
 
 	/* restoration of values of USP and SSP */
 	/* restore the ssp's value recorded in this task's TCB & USP value recorded in the ss*/
-	adda.l	#4, %a1		/* get stack pointer */
-	move.l	(%a1), %sp	/* restore stack pointer */
+	move.l	4(%a1), %sp	/* restore stack pointer */
 	move.l	(%sp)+, %a2
 	move.l	%a2, %USP	/* restore USP */
 	
 
 	/* restoration of al of remained registers */
 	/* restore the values of remained 15 registers piled up on the supervisor's stack*/
-	movem.l	(%sp)+, %d0-%d7/%a0-%a7
+	movem.l	(%sp)+, %d0-%d7/%a0-%a6
 
 	/* start of user task */
 	/* execute the RTE instruction*/
@@ -105,12 +106,13 @@ hard_clock: /* timer interrupt routine */
 	addi.w 	#0x2000,%d1
 	cmpi.w 	#0x2000,%d1 	/*check if supervisor mode*/
 	beq 	hard_clock_end
+	
 	/* add "curr_task" to the end of 'ready' using addq() */
 	move.l 	curr_task,-(%sp)
 	move.l 	#ready, -(%sp)
 	jsr 	addq
+	
 	add.l 	#8, %sp
-
 	/* start "sched": the ID of task to be executed next='next_task' */
 	jsr sched
 	/* start 'swtch'*/
@@ -120,7 +122,9 @@ hard_clock_end:
 	movem.l (%sp)+, %d0-%d1/%a1
 	rts
 
-init_timer:	/* clock interrupt routine: generates hardware interruption by the timer control routine(created in jikken1): interruption period: 1s) */
+init_timer:
+	/* clock interrupt routine: generates hardware interruption by the timer control routine */
+	/* interruption period: 1s */
 	movem.l %d0-%d2,-(%sp)
 	
 	move.l	#SYSCALL_NUM_RESET_TIMER, %d0
@@ -166,10 +170,11 @@ V:
 ********************************
 pv_handler:
 	movem.l	%d1, -(%sp)	/* save argument on top of stack */
-	move.w	#0x2700, %SR
-	cmp.l	#0, %d0
+	move.w	#0x2700, %SR	/* supervisor mode */
+	
+	cmpi.l	#0, %d0
 	beq	CALL_P_BODY
-	cmp.l	#1, %d0
+	cmpi.l	#1, %d0
 	beq 	CALL_V_BODY
 	bra	end_pv_handler
 CALL_P_BODY:
